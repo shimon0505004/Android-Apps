b27807455e53da9396f670021c66d87df1a39676
Shaikh Shawon Arefin Shimon <shimon0505004@gmail.com>
2019-03-17 01:15:01 +0600

    Added New Activity for Complete Work History

--- Kotlin/CoolResume/app/src/main/java/com/shawonarefin/coolresume/MainActivity.kt
+++ Kotlin/CoolResume/app/src/main/java/com/shawonarefin/coolresume/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.shawonarefin.coolresume
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var workHistory = findViewById<Button>(R.id.WorkHistory_Button)
+        workHistory.setOnClickListener {
+            println("Hey you hit the button!")
+
+            var moveToWorkHistory = Intent(getApplicationContext(),WorkHistoryActivity::class.java)
+            startActivity(moveToWorkHistory)
+        }
     }
 }
