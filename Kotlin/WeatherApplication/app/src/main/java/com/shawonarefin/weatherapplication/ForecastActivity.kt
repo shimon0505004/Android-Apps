fd3a57a126a02c547cddbb458bd32d67541d7e74
Shaikh Shawon Arefin Shimon <shimon0505004@gmail.com>
2019-03-17 15:23:28 +0600

    In the forecast activity, added listView to display list containing 7 day forecast of a particular city. Initially created a list of strings using kotlin's listOf() feature. Then, using an array adapter, converted this list into a array adapter so that the list view can accept the list of strings.
    Initially the list of strings contain random strings.

--- Kotlin/WeatherApplication/app/src/main/java/com/shawonarefin/weatherapplication/ForecastActivity.kt
+++ Kotlin/WeatherApplication/app/src/main/java/com/shawonarefin/weatherapplication/ForecastActivity.kt
@@ -2,11 +2,39 @@ package com.shawonarefin.weatherapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class ForecastActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
+
+        var listView = findViewById<ListView>(R.id.forecast_ListView)
+        var randomThings = listOf<String>("Hello"
+            , "How Are You"
+            , "I like Cheese"
+            , "Hello"
+            , "How Are You"
+            , "I like Cheese"
+            , "Hello"
+            , "How Are You"
+            , "I like Cheese"
+            , "Hello"
+            , "How Are You"
+            , "I like Cheese"
+            , "Hello"
+            , "How Are You"
+            , "I like Cheese"
+            , "Hello"
+            , "How Are You"
+            , "I like Cheese"
+            , "Hello"
+            , "How Are You"
+            , "I like Cheese"
+        )
+        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,randomThings)
+        listView.adapter = adapter
     }
 }
