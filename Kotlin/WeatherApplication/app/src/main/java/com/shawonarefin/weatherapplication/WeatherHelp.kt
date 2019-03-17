0684f33e5db12083f0dc56c63b6ce89077a114ea
Shaikh Shawon Arefin Shimon <shimon0505004@gmail.com>
2019-03-17 20:35:46 +0600

    Added some classes based on JSON description from OpenWeatherMap API responses. Might be modified later on.

--- Kotlin/WeatherApplication/app/src/main/java/com/shawonarefin/weatherapplication/WeatherHelp.kt
+++ Kotlin/WeatherApplication/app/src/main/java/com/shawonarefin/weatherapplication/WeatherHelp.kt
@@ -11,13 +11,19 @@ interface WeatherAPI{
     fun getForecast() : Call<Weather>
 }
 
-class Weather
+class Weather(val weatherList : WeatherList)
+class WeatherList(val weatherDescription : WeatherDescription , val dateOfWeather : DateOfWeather)
+class WeatherDescription(val mainWeather: MainWeatherText, val detailedWeather: DetailedWeatherText)
+class DateOfWeather
+class MainWeatherText
+class DetailedWeatherText
 
 
 class WeatherRetriever{
     val service : WeatherAPI
 
     init {
+
         val retrofit = Retrofit.Builder().baseUrl("https://api.myjson.com/").addConverterFactory(GsonConverterFactory.create()).build()
         service = retrofit.create(WeatherAPI::class.java)
     }
