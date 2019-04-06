5a8fe87c7726da61650d1a35aa2da1d311ccf3d0
Shaikh Shawon Arefin Shimon <shimon0505004@gmail.com>
2019-04-06 19:20:34 +0600

    added searching capability for FindRepo application.

--- Kotlin/FindRepo/app/src/main/java/com/shawonarefin/findrepo/SearchResultActivity.kt
+++ Kotlin/FindRepo/app/src/main/java/com/shawonarefin/findrepo/SearchResultActivity.kt
@@ -8,5 +8,7 @@ class SearchResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_result)
+        val searchTerm = intent.getStringExtra("searchTerm")
+        println(searchTerm)
     }
 }
