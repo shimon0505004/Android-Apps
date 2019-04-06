8f71703ede2f08179fba99081ac8e5613389fc1b
Shaikh Shawon Arefin Shimon <shimon0505004@gmail.com>
2019-04-06 13:41:41 +0600

    Added Textbox, checkbox and button to AddToDoList activity.

--- Kotlin/TodoList/app/src/main/java/com/shawonarefin/todolist/AddTodoActivity.kt
+++ Kotlin/TodoList/app/src/main/java/com/shawonarefin/todolist/AddTodoActivity.kt
@@ -8,5 +8,7 @@ class AddTodoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_todo)
+
+        title = "New To Do"
     }
 }
