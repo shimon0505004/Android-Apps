52f06b8f9ca085c8688ec2fa4098104223297522
Shaikh Shawon Arefin Shimon <shimon0505004@gmail.com>
2019-04-15 19:31:52 +0600

    Added showToast and countUp functions in MainActivity.kt
    (https://codelabs.developers.google.com/codelabs/android-training-layout-editor-part-a)
    
    Signed-off-by: Shaikh Shawon Arefin Shimon <shimon0505004@gmail.com>

---
+++ Kotlin/HelloToast/app/src/main/java/com/shawonarefin/hellotoast/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.shawonarefin.hellotoast
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun showToast(){
+
+    }
+
+    fun countUp(){
+
+    }
+}
