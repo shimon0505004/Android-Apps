264dce8ab8d26eac10a9e0b853379f039210c6eb
SHAIKH SHAWON AREFIN SHIMON <shimon0505004@gmail.com>
2019-08-05 11:18:23 +0600

    Creating first flutter application

---
+++ Flutter/second_app/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    new Center(
+      child:  new Text("This is my first flutter application", textDirection: TextDirection.ltr,)
+    )
+  );
+}
