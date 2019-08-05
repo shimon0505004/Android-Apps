02b7b68699f5a8e9a85d6257016decf9fdcb104b
SHAIKH SHAWON AREFIN SHIMON <shimon0505004@gmail.com>
2019-08-05 16:11:43 +0600

    Created a list of array in stateful widget

--- Flutter/third_app/lib/main.dart
+++ Flutter/third_app/lib/main.dart
@@ -4,38 +4,40 @@ void main(){
   runApp(new application());
 }
 
-class application extends StatelessWidget {
+class application extends StatefulWidget {
+  @override
+  _applicationState createState() => _applicationState();
+}
+
+class _applicationState extends State<application> {
+  List<int> items = new List();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    for(int i=0; i<50; i++){
+      items.add(i);
+    }
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "Shimon App",
+      title: 'Dummy Application',
       home: new Scaffold(
-        appBar: new AppBar(title: new Text('Row & Column'),),
-        body: new ListView(
-          children: <Widget>[
-            new ListTile(
-              title: new Text('item 1'),
-              trailing: new Icon(Icons.arrow_forward),
-            ),
-            new ListTile(
-              title: new Text('item 2'),
-              trailing: new Icon(Icons.arrow_forward),
-            ),
-            new ListTile(
-              title: new Text('item 3'),
-              trailing: new Icon(Icons.arrow_forward),
-            ),
-            new ListTile(
-              title: new Text('item 4'),
-              trailing: new Icon(Icons.arrow_forward),
-            ),
-            new ListTile(
-              title: new Text('item 5'),
-              trailing: new Icon(Icons.arrow_forward),
-            ),
-          ],
-        )
+        appBar: new AppBar(title: new Text('List Widget'),),
+        body: new ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (BuildContext context, int index){
+              return new ListTile(
+                title: new Text('item no : $index'),
+                trailing: new Icon(Icons.arrow_forward),
+              );
+            }
+        ),
       )
     );
   }
 }
+
